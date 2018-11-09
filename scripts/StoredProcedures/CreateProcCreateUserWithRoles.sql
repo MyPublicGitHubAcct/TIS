7f3c58871d2f52bb39bda02f6f50773a2d14308b
Mark <>
2018-11-09 16:20:32 -0500

    Updated sp_CreateUserWithRoles

--- scripts/StoredProcedures/CreateProcCreateUserWithRoles.sql
+++ scripts/StoredProcedures/CreateProcCreateUserWithRoles.sql
@@ -1,8 +1,8 @@
 /**
     Create a new stored procedure called 'sp_CreateUserWithRoles' in schema 'tis'
-    1 - make two temp tables - one for Details & one for Roles
+    1 - make temp table
     2 - add the details to AppUser
-    3 - get the new user's id
+    3 - get the new user's id & make second temp table from the first
     4 - add the role settings to UserRoles
 */
 
@@ -20,12 +20,8 @@ CREATE PROCEDURE tis.sp_CreateUserWithRoles
     @responseMessage VARCHAR(250) = '' OUTPUT
 AS
   BEGIN
-
---   INSERT INTO temp_details
---   INSERT INTO temp_roles
-
-    -- bring in entire json document.
-    SELECT * FROM OPENJSON ( @json ) 
+    -- bring in whole json document.
+    SELECT temp_d.* INTO #temp_details FROM OPENJSON ( @json ) 
     WITH (
         FirstName VARCHAR(200) '$.Details.FirstName',
         LastName VARCHAR(200) '$.Details.LastName',
@@ -36,9 +32,81 @@ AS
         IsManager BIT '$.Details.isManager',
         IsActive BIT '$.Details.isActive',
         Perms NVARCHAR(MAX) '$.Roles' AS JSON
-    )
+    ) AS temp_d
+
+    -- ///////////// ADD USER
+    -- test: if logon already exists, return 'failed'
+    --       else insert user/roles and return 'success'
+    IF EXISTS (SELECT Logon FROM [tis].[AppUser] WHERE Logon=(SELECT Logon FROM #temp_details))
+        SET @responseMessage='failed'
+    ELSE
+        BEGIN TRY
+            DECLARE @Salt UNIQUEIDENTIFIER=NEWID()
+            DECLARE @DateChanged DATE = GETDATE()
+            
+            INSERT INTO [tis].[AppUser]
+            (
+                [FirstName], [LastName], [Manager], [Logon], [Password], 
+                [Salt], [Department], [isManager], [isActive], [DateChanged]
+            )
+            VALUES
+            (
+                (SELECT FirstName FROM #temp_details),
+                (SELECT LastName FROM #temp_details),
+                (SELECT Manager FROM #temp_details),
+                (SELECT Logon FROM #temp_details),
+                HASHBYTES('SHA2_512', (SELECT Pwd FROM #temp_details)+CAST(@Salt AS VARCHAR(36))), 
+                @Salt, 
+                (SELECT Department FROM #temp_details),
+                (SELECT isManager FROM #temp_details),
+                (SELECT isActive FROM #temp_details),
+                @DateChanged
+            )
+                -- ///////////// ADD USER'S ROLES
+                DECLARE @UserId INT = (SELECT ID FROM [tis].[AppUser] WHERE Logon=(SELECT Logon FROM #temp_details));
+
+                SELECT temp_r.* INTO #temp_roles FROM OPENJSON ( @json ) 
+                WITH (
+                    Perms NVARCHAR(MAX) '$.Roles' AS JSON
+                ) AS temp_r
+
+                INSERT INTO [tis].[UserRole]
+                SELECT @UserId AS UserId, tempPerm.*
+                FROM #temp_roles CROSS APPLY OPENJSON(Perms)
+                WITH
+                (
+                    RoleId INT '$.RoleId',
+                    UserHas BIT '$.UserHas'
+                ) AS tempPerm
+
+                SET @responseMessage='success'
+
+                -- ///////////// CLEANUP
+                DROP TABLE #temp_roles
+
+        END TRY
+        BEGIN CATCH
+            SET @responseMessage=ERROR_MESSAGE()
+        END CATCH
 
 
+    -- ///////////// ADD USER'S ROLES
+    -- SELECT temp_r.* INTO #temp_roles FROM OPENJSON ( @json ) 
+    -- WITH (
+    --     Perms NVARCHAR(MAX) '$.Roles' AS JSON
+    -- ) AS temp_r
 
+    -- INSERT INTO [tis].[UserRole]
+    -- SELECT @UserId AS UserId, tempPerm.*
+    -- FROM #temp_roles CROSS APPLY OPENJSON(Perms)
+    -- WITH
+    -- (
+    --     RoleId INT '$.RoleId',
+    --     UserHas BIT '$.UserHas'
+    -- ) AS tempPerm
+
+    -- ///////////// CLEANUP
+    DROP TABLE #temp_details
+    -- DROP TABLE #temp_roles
   END
 GO
